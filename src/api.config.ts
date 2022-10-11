b9273fbd036a7b25792c9910630721dc3c97fae4
Theshawa Dasun <93116160+Theshawa@users.noreply.github.com>
2022-10-11 23:28:07 +0530

    dev_help source

--- src/api.config.ts
+++ src/api.config.ts
@@ -1,3 +1,3 @@
 export const API_KEY = `9754364cf130d17803f95d150e75fcfc`;
 
-export const DATA_SOURCE = `https://github.com/Theshawa/dev_help/countries-n-cities-list.json`;
+export const DATA_SOURCE = `https://raw.githubusercontent.com/Theshawa/dev_help/main/countries-n-cities-list.json`;
